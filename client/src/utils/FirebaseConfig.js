169695f256514cdb4b6cb529dc66a2264dfc850d
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-07-24 19:41:20 +0530

    .env file added

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjmPA2OerjwWpmU8h17aVNPRFkz6EPViY",
-    authDomain: "whatsappclone-c59ce.firebaseapp.com",
-    projectId: "whatsappclone-c59ce",
-    storageBucket: "whatsappclone-c59ce.appspot.com",
-    messagingSenderId: "94467007243",
-    appId: "1:94467007243:web:50e42a9f2ee4c0081b380e",
-    measurementId: "G-W5Y7FM0NS7"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
